ba1664af16639329963a15440ae5a3f80ea612a9
Nathaniel Bechard <natebechard@gmail.com>
2021-01-09 21:18:06 -0500

    defied objects for the robot

--- robot_code/robot_code.ino
+++ robot_code/robot_code.ino
@@ -1,6 +1,14 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <SPI.h>
+#include <SD.h>
+#include <Ultrasonic.h>
+#include <Servo.h>
 
+Servo scan_servo;
+Ultrasonic dist(6, 7); //ultrasonic sensor on pins 6 and 7
+
+void setup() {
+  scan_servo.attach(5); // servo on pin 5
+  SD.begin(10); // sd card on pin 10, plus the spi port
 }
 
 void loop() {
