c1d75fb03c80c9e84e136f85a4de4436a955911a
Nathaniel Bechard <natebechard@gmail.com>
2021-01-10 12:20:55 -0500

    added Serial ports to the esp32 code

--- door_detection_esp32_live/door_detection_esp32_live.ino
+++ door_detection_esp32_live/door_detection_esp32_live.ino
@@ -1,18 +1,26 @@
 #include <EloquentTinyML.h>
 #include "bytearray_full_model.h" // in the same directory as this sketch is the model
+#include "arduino.h"
 
 #define NUMBER_OF_INPUTS 90
 #define NUMBER_OF_OUTPUTS 1
 #define TENSOR_ARENA_SIZE 128*64
 
-Eloquent::TinyML::TfLite<NUMBER_OF_INPUTS, NUMBER_OF_OUTPUTS, TENSOR_ARENA_SIZE> ml(bytearray_full_model);
+#define RX 16
+#define TX 17
+
+
+
+Eloquent::TinyML::TfLite<NUMBER_OF_INPUTS, NUMBER_OF_OUTPUTS, TENSOR_ARENA_SIZE> ml;
 
 void setup() {
-  // put your setup code here, to run once:
-
+  Serial.begin(9600);
+  Serial2.begin(2400, SERIAL_8N1, RX, TX);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  while(Serial2.available()>0){
+    Serial.print(Serial2.read());
+  }
 
 }
