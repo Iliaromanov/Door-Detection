a17a0ecd27610e3e26c818bbc584dcd6ecc6df10
Nathaniel Bechard <natebechard@gmail.com>
2021-01-10 10:45:44 -0500

    general update:

--- door_detection_esp32_live/door_detection_esp32_live.ino
+++ door_detection_esp32_live/door_detection_esp32_live.ino
@@ -1,4 +1,11 @@
 #include <EloquentTinyML.h>
+#include "bytearray_full_model.h" // in the same directory as this sketch is the model
+
+#define NUMBER_OF_INPUTS 90
+#define NUMBER_OF_OUTPUTS 1
+#define TENSOR_ARENA_SIZE 128*64
+
+Eloquent::TinyML::TfLite<NUMBER_OF_INPUTS, NUMBER_OF_OUTPUTS, TENSOR_ARENA_SIZE> ml(bytearray_full_model);
 
 void setup() {
   // put your setup code here, to run once:
